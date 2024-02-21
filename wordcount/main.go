aca378980d0bc163a6e1e7bf420effefbc80b9cc
Mark Gurianov <mark.gurianov@gmail.com>
2024-02-21 19:27:11 +0300

    Solved wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -15,11 +15,9 @@ func check(e error) {
 }
 
 func main() {
-	fp := os.Args[1:]          // file's paths
 	wc := make(map[string]int) // words count
-	var w []string
 
-	for _, filePath := range fp {
+	for _, filePath := range os.Args[1:] {
 		file, err := os.Open(filePath)
 		check(err)
 
@@ -27,21 +25,16 @@ func main() {
 		fs.Split(bufio.ScanLines)
 
 		for fs.Scan() {
-			row := fs.Text()
-			if count, found := wc[row]; found {
-				nr := count + 1
-				wc[row] = nr
-				if nr == 2 {
-					w = append(w, row)
-				}
-			} else {
-				wc[row] = 1
-			}
+			word := fs.Text()
+			wc[word]++
 		}
 		check(file.Close())
 	}
 
-	for _, word := range w {
-		fmt.Printf("%d\t%s\n", wc[word], word)
+	for word, count := range wc {
+		if count < 2 {
+			continue
+		}
+		fmt.Printf("%v\t%v\n", count, word)
 	}
 }
