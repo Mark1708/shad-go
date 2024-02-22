01d1db51d42ce1c34caaca7afbbd99865eecfd66
Mark Gurianov <mark.gurianov@gmail.com>
2024-02-22 09:49:36 +0300

    Solved fetchall

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,48 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
 
+type res struct {
+	url      string
+	duration time.Duration
+}
+
+func main() {
+	strings := os.Args[1:]
+	start := time.Now()
+	respCh := make(chan res, len(strings))
+	wg := &sync.WaitGroup{}
+
+	for _, url := range strings {
+		wg.Add(1)
+		go fetch(url, respCh, wg)
+	}
+	wg.Wait()
+	close(respCh)
+
+	for resp := range respCh {
+		fmt.Printf("Url: %s\tDuration: %v\n", resp.url, resp.duration)
+	}
+
+	fmt.Println(time.Since(start))
+}
+
+func fetch(url string, respCh chan res, wg *sync.WaitGroup) {
+	now := time.Now()
+
+	resp, err := http.Get(url)
+	if err == nil {
+		defer resp.Body.Close()
+
+	}
+
+	respCh <- res{url: url, duration: time.Since(now)}
+	wg.Done()
 }
