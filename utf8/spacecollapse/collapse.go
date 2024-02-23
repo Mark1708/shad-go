20c5a31e5bf2d606799bd2c77718c3931e7b4323
Mark Gurianov <mark.gurianov@gmail.com>
2024-02-23 12:34:29 +0300

    Solved utf8spacecollapse

--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,30 @@
 
 package spacecollapse
 
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 func CollapseSpaces(input string) string {
-	return ""
+	var sb strings.Builder
+	sb.Grow(len(input))
+
+	prevSpace := false
+	for len(input) > 0 {
+		r, size := utf8.DecodeRuneInString(input)
+		input = input[size:]
+
+		isSpace := unicode.IsSpace(r)
+		if !isSpace {
+			sb.WriteRune(r)
+		} else if !prevSpace {
+			sb.WriteRune(' ')
+		}
+		prevSpace = isSpace
+
+	}
+
+	return sb.String()
 }
